0499f4928c80d24a5eab02529652b97ad4f13627
luizarnoldch <luizarnoldch@gmail.com>
2023-12-02 13:43:33 -0500

    cf + cli + sdk fix 7

--- IAM/practice/SDK/create_user/test.sh
+++ IAM/practice/SDK/create_user/test.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-LAMBDAS=("get_company_by_id" "get_companys")
-
 test_lambda() {
-  for folder in "${LAMBDAS[@]}"; do
-    (
+      cd "SDK/create_user" || exit
       aws lambda invoke --function-name IAM_practice_CreateUser --payload file://input.json ./output.json
       echo -e "\n"
       cat ./output.json
       echo -e "\n"
-    )
-  done
 }
 
 test_lambda
