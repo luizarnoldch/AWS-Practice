d013d3ef9072633102e65fbfe24869f9de276ecf
luizarnoldch <luizarnoldch@gmail.com>
2023-12-04 20:44:18 -0500

    test fix

--- IAM/group/SDK/create_group/test.sh
+++ IAM/group/SDK/create_group/test.sh
@@ -2,7 +2,7 @@
 
 test_lambda() {
       cd "SDK/create_group" || exit
-      aws lambda invoke --function-name IAM_practice_CreateUser --payload file://input.json --cli-binary-format raw-in-base64-out ./output.json
+      aws lambda invoke --function-name IAM_practice_CreateGroup --payload file://input.json --cli-binary-format raw-in-base64-out ./output.json
       echo -e "\n"
       cat ./output.json
       echo -e "\n"
