21772d7b0e874bd7bcb5dd0738717465d3bde7a5
luizarnoldch <luizarnoldch@gmail.com>
2023-12-04 21:02:03 -0500

    delete group

---
+++ IAM/group/SDK/delete_group/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export GOOS="linux"
+export GOARCH="amd64"
+export CGO_ENABLED="0"
+
+build_lambda() {
+  cd "SDK/delete_group" || exit
+  go build -o bootstrap -tags lambda.norpc
+  zip ./main.zip bootstrap
+  rm -rf bootstrap
+}
+
+build_lambda
